7b44490b3b581910a78c8aa5ece245883ce24c15
Radek Krahl <radek@krahl.pl>
2017-12-23 23:54:40 +0100

    Moved processor flags enum to separate file, added some helper functions.

--- 6502/Instructions.cs
+++ 6502/Instructions.cs
@@ -17,23 +17,8 @@ namespace C64Emu._6502
         public static void LDA(Operand op, Cpu cpu)
         {
             cpu.A = op.Value;
-            if (op.Value == 0)
-            {
-                cpu.P |= ProcessorStatus.Z;
-            }
-            else
-            {
-                cpu.P &= ~ProcessorStatus.Z;
-            }
-
-            if ((op.Value & 0x80) != 0)
-            {
-                cpu.P |= ProcessorStatus.N;
-            }
-            else
-            {
-                cpu.P &= ~ProcessorStatus.N;
-            }
+            var p = op.Value == 0 ? cpu.P.Set(ProcessorStatus.Z) : cpu.P.Clear(ProcessorStatus.Z);
+            cpu.P = (op.Value & 0x80) != 0 ? p.Set(ProcessorStatus.N) : p.Clear(ProcessorStatus.N);
         }
     }
 }
---
+++ 6502/ProcessorStatus.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C64Emu._6502
+{
+    [Flags]
+    public enum ProcessorStatus
+    {
+        /// <summary>
+        /// Negative flag
+        /// </summary>
+        N = 0x80,
+
+        /// <summary>
+        /// oVerflow flag
+        /// </summary>
+        V = 0x40,
+
+        /// <summary>
+        /// Unused
+        /// </summary>
+        _ = 0x20,
+
+        /// <summary>
+        /// Break flag
+        /// </summary>
+        B = 0x10,
+
+        /// <summary>
+        /// Decimal mode flag
+        /// </summary>
+        D = 0x08,
+
+        /// <summary>
+        /// Interrupt disable flag
+        /// </summary>
+        I = 0x04,
+
+        /// <summary>
+        /// Zero flag
+        /// </summary>
+        Z = 0x02,
+
+        /// <summary>
+        /// Carry flag
+        /// </summary>
+        C = 0x01
+    }
+
+    public static class ProcessorStatusExtensions
+    {
+        public static ProcessorStatus Clear(this ProcessorStatus self, ProcessorStatus flag)
+        {
+            var copy = self;
+            copy &= ~flag;
+            return copy;
+        }
+
+        public static ProcessorStatus Set(this ProcessorStatus self, ProcessorStatus flag)
+        {
+            var copy = self;
+            copy |= flag;
+            return copy;
+        }
+    }
+}
